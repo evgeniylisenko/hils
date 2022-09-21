bf526cc245e296940b7408b4a527943591dd9715
evgeniy lisenko <lisenko.ocdev@gmail.com>
2022-09-21 12:24:25 +0300

    dz chek

--- hils_leson_form.php
+++ hils_leson_form.php
@@ -13,11 +13,11 @@
 	<input type="text" id="username" name="name" placeholder="Введите свое имя" value="">
 	<input type="password" id="password" name="password" placeholder="Введите свой пароль" value="">
 	<br>
-	<span>Я соглашаюсь на обработку данных</span> <input type="checkbox" name="dataprocessing[]" value="">
+	<span>Я соглашаюсь на обработку данных</span> <input type="checkbox" name="dataprocessing" value="yes">
 	<br>
 	<p> Укажите свой пол </p>
-	<span> Мужской </span><input type="radio" name="sex[]" value="">
-	<span> Женский </span><input type="radio" name="sex[]" value="">
+	<span> Мужской </span><input type="radio" name="sex" value="men">
+	<span> Женский </span><input type="radio" name="sex" value="women">
 	<br>
 	<button type="submit">Отправить</button>
 </form>
